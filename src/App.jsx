019aac383059c95a1c2f223a9a76981cab3cfb4d
Saily Hurtado <sailyhurtado@gmail.com>
2025-04-28 11:31:06 +0200

    Creating the Die component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import './App.css'
+import Die from './components/Die'
 
 export default function App() {
-
-  return (
-    <main>
-    </main>
+    
+    return (
+        <main>
+            <div className='dice-container'>
+                <Die value={Math.floor(Math.random() * 6) + 1} />
+                <Die value={Math.floor(Math.random() * 6) + 1} />
+                <Die value={Math.floor(Math.random() * 6) + 1} />
+                <Die value={Math.floor(Math.random() * 6) + 1} />
+                <Die value={Math.floor(Math.random() * 6) + 1} />
+                <Die value={Math.floor(Math.random() * 6) + 1} />
+                <Die value={Math.floor(Math.random() * 6) + 1} />
+                <Die value={Math.floor(Math.random() * 6) + 1} />
+                <Die value={Math.floor(Math.random() * 6) + 1} />
+                <Die value={Math.floor(Math.random() * 6) + 1} />
+            </div>            
+        </main>
   )
 }
 
---
+++ src/components/Die.jsx
@@ -0,0 +1,6 @@
+
+export default function Die(props) {
+    return (
+        <button>{props.value}</button>
+    )
+}
