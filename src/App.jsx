83953b3b782d40ae870848160cc2449e04001650
Saily Hurtado <sailyhurtado@gmail.com>
2025-04-28 15:44:03 +0200

    /**      * Challenge:      * Make the confetti drop when the game is won! 🎉🎊      */

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState} from 'react'
 import './App.css'
 import Die from './components/Die'
 import { nanoid } from 'nanoid';
+import Confetti from 'react-confetti'
 
 export default function App() {
     
@@ -48,6 +49,7 @@ export default function App() {
 
     return (
         <main>
+            {gameWon && <Confetti />}
             <h1 className="title">Tenzies</h1>
             <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
             <div className='dice-container'>                
@@ -56,8 +58,7 @@ export default function App() {
             
             <button className='roll-dice' onClick={rollDice}>
                 {gameWon ? "New Game" : "Roll"}                
-            </button>
-                     
+            </button>                     
         </main>
   )
 }
