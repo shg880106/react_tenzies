aae8b2dc3d2f07a0cdfca8293ddc1653ed0c1ded
Saily Hurtado <sailyhurtado@gmail.com>
2025-04-28 11:50:46 +0200

    Generate 10 ramdom numbers in a function called: generateAllNewDice()

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,16 @@ import Die from './components/Die'
 
 export default function App() {
     
+    function generateAllNewDice() {
+        return new Array(10)
+            .fill(0)
+            .map(() => Math.ceil(Math.random() * 6))
+    }
+
+    console.log(generateAllNewDice())
     return (
         <main>
-            <div className='dice-container'>
+            <div className='dice-container'>                
                 <Die value={Math.floor(Math.random() * 6) + 1} />
                 <Die value={Math.floor(Math.random() * 6) + 1} />
                 <Die value={Math.floor(Math.random() * 6) + 1} />
