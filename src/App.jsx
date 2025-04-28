8f89efc976575ac34cd2be255725fe69a6f020a9
Saily Hurtado <sailyhurtado@gmail.com>
2025-04-28 13:16:40 +0200

    /**  * Challenge:  *  * Create state to hold our array of numbers. (Initialize  * the state by calling our `generateAllNewDice` function so it  * loads all new dice as soon as the app loads)  *  * Map over the state numbers array to generate our array  * of Die components and render those in place of our  * manually-written 10 Die elements.  */

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,25 @@
+import { useState } from 'react'
 import './App.css'
 import Die from './components/Die'
 
 export default function App() {
     
+    const [dice, setDice] = useState(generateAllNewDice())
+
     function generateAllNewDice() {
         return new Array(10)
             .fill(0)
             .map(() => Math.ceil(Math.random() * 6))
     }
 
-    console.log(generateAllNewDice())
+    const diceElements = dice.map(num => 
+        <Die value={num} />
+    )
+
     return (
         <main>
             <div className='dice-container'>                
-                <Die value={Math.floor(Math.random() * 6) + 1} />
-                <Die value={Math.floor(Math.random() * 6) + 1} />
-                <Die value={Math.floor(Math.random() * 6) + 1} />
-                <Die value={Math.floor(Math.random() * 6) + 1} />
-                <Die value={Math.floor(Math.random() * 6) + 1} />
-                <Die value={Math.floor(Math.random() * 6) + 1} />
-                <Die value={Math.floor(Math.random() * 6) + 1} />
-                <Die value={Math.floor(Math.random() * 6) + 1} />
-                <Die value={Math.floor(Math.random() * 6) + 1} />
-                <Die value={Math.floor(Math.random() * 6) + 1} />
+                {diceElements}
             </div>            
         </main>
   )
