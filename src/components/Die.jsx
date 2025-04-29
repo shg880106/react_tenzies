49eb535396f2e0a5e7cb6751750618bef2524749
Saily Hurtado <sailyhurtado@gmail.com>
2025-04-29 09:35:08 +0200

    Adding details to improve app accessibility

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -7,7 +7,9 @@ export default function Die(props) {
     return (
         <button 
             style={styles} 
-            onClick={() => props.hold(props.id)}>
+            onClick={() => props.hold(props.id)}
+            aria-pressed={props.isHeld}
+            aria-label={`Die with value ${props.value}, ${props.isHeld ? "held" : "not held"}`}>
                 {props.value}
         </button>
     )
